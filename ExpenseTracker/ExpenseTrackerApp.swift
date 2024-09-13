a310d7e4dfe5df0477a9158de4802a740bdc2e5c
Jack Bodworth <jack.bodsworth@unimelb.edu.au>
2024-09-13 10:24:21 +1000

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Jack Bodsworth on 13/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
